cfe902a508686cb4c75908dc56f37fdce930f66e
jmargal <jmargal25@gmail.com>
2022-11-15 18:54:52 +0100

    Primer angular

---
+++ src/app/views/listado/entrada/entrada.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Entrada } from '../../../shared/interfaces/entrada';
+
+@Component({
+  selector: 'app-entrada',
+  templateUrl: './entrada.component.html',
+  styleUrls: ['./entrada.component.css'],
+})
+export class EntradaComponent implements OnInit {
+  @Input()
+  public entrada: Entrada;
+
+  constructor() {
+    this.entrada = {
+      titulo: '',
+      resumen: '',
+    };
+  }
+
+  ngOnInit(): void {}
+}
